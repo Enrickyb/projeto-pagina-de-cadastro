db18e0f813d32116e304122daed5ed610fd5ae48
Enrickyb <80066756+Enrickyb@users.noreply.github.com>
2022-03-16 16:07:38 -0400

    add validation

--- js/login.js
+++ js/login.js
@@ -1,10 +1,12 @@
-const email = document.getElementById('email')
-const password = document.getElementById('password')
+
 const validation = document.getElementById('submit')
 
 validation.addEventListener("click", valid)
 
     function valid() {
+        const email = document.getElementById('email').value
+        const password = document.getElementById('password').value
+
         if(email == 'enrickyb@hotmail.com' && password == '1234'){
             alert('Login sucessful')
             return false
